d0638c687cfb65e3ec504bee262cdfa910683248
Pratyush-Shanbhag <pratyushshanbhag10@gmail.com>
2022-03-24 12:01:31 -0700

    added code to calculate amount earned

--- Sales.cpp
+++ Sales.cpp
@@ -15,7 +15,6 @@ Sales::Sales()
 
 Sales::Sales(string inputId, int inputYear, string inputName, int inputAmount)
 {
-   /* Write your code here */
    id = inputId;
    year = inputYear;
    name = inputName;
@@ -24,11 +23,34 @@ Sales::Sales(string inputId, int inputYear, string inputName, int inputAmount)
 
 void Sales::display() const
 {
-   /* Write your code here */
    cout << setw(15) << "ID: " << id << endl
         << setw(15) << "Year: " << year << endl
         << setw(15) << "Name: " << name << endl
         << setw(16) << "Amount Sold: $" << amountSold << endl
         << setw(15) << "Commission: " << detCommissionPercent() * 100 << "%" << endl
         << setw(16) << "Amount Earned: $" << detAmountEarned() << endl;
+}
+
+double Sales::detCommissionPercent() const
+{
+   double commission = 0;
+    
+   if ( amountSold > 0)
+   {
+      if(amountSold <= 1000)
+         commission = 0.03;
+      else if(amountSold <= 5000)
+         commission = 0.045;
+      else if(amountSold <= 10000)
+         commission = 0.0525;
+      else
+         commission = 0.06;  
+   }
+    
+   return commission;
+}
+
+double Sales::detAmountEarned() const
+{    
+   return amountSold * detCommissionPercent();
 }
