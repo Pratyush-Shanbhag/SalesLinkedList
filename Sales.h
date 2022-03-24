3faf5579cbf912b1545f3f15ec788809b640039a
Pratyush-Shanbhag <pratyushshanbhag10@gmail.com>
2022-03-23 18:39:29 -0700

    added constructors and accessors and mutators to Sales header file

--- Sales.h
+++ Sales.h
@@ -7,12 +7,25 @@ using std::string;
 
 class Sales
 {
-   private:
-      string id;
-      int year;
-      string name;
-      int amountSold;
+    private:
+        string id;
+        int year;
+        string name;
+        int amountSold;
 
+    public:
+        Sales();
+        Sales(string inputId, int inputYear, string inputName, int inputAmountSold);
+   
+        void setId(string inputId) { id = inputId; }
+        void setYear(int inputYear) { year = inputYear; }
+        void setName(string inputName) { name = inputName; }
+        void setAmount(int inputAmount) { amountSold = inputAmount; }
+   
+        string getId() { return id; }
+        int getYear() { return year; }
+        string getName() { return name; }
+        int getAmount() { return amountSold; }
 };
 
 #endif
