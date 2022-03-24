747fc605387afa435bfd8fd503c9d23de48f6dfa
Pratyush-Shanbhag <pratyushshanbhag10@gmail.com>
2022-03-23 18:35:44 -0700

    added code to Sales header file

---
+++ Sales.h
@@ -0,0 +1,18 @@
+#ifndef SALES_H
+#define SALES_H
+#include <string>
+
+using std::ostream;
+using std::string;
+
+class Sales
+{
+   private:
+      string id;
+      int year;
+      string name;
+      int amountSold;
+
+};
+
+#endif
