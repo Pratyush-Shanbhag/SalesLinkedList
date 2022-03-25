b3d17ce4bb9ecbaf81c1bcf7336c4d1ad93e2369
Pratyush-Shanbhag <pratyushshanbhag10@gmail.com>
2022-03-25 16:30:09 -0700

    added searchList() and displayList() to main.cpp

--- main.cpp
+++ main.cpp
@@ -84,3 +84,79 @@ void deleteList(LinkedList &list)
     }
     cout << "___________________END DELETE SECTION_____" << endl;
 }
+
+void searchList(const LinkedList &list)
+{
+    string target = "";
+    Sales obj;
+
+    cout << " Search" << endl;
+    cout << "========" << endl;
+
+   /* Write your code here */
+    while(target != "Q")
+    {
+        cout << "Enter a name (or Q to stop searching):" << endl;
+        getline(cin, target);
+        target[0] = toupper(target[0]);
+        if(target != "Q")
+        {
+            Sales obj;
+            if(list.searchList(target, obj))
+            {
+               obj.display();
+            }
+            else
+                cout << target << " not found!" << endl;
+        }
+    }   
+   
+    cout << "___________________END SEARCH SECTION _____" << endl;
+}
+
+/*
+Display manager: all, average, or year hired reports including header and footer where appropriate,
+ depending on the user's choice;
+ displays the number of nodes (always)
+Input Parameter: list
+*/
+void displayList(const LinkedList &list)
+{
+    // Sub-functions of displayManager()
+    void showMenu(void);
+    string getOption(void);
+    void showHeader(string line);
+   
+    
+    string line = "==== ==================== =============\n";
+    string option;
+    
+    showMenu();
+    option = getOption();
+    while(option[0] != 'Q')
+    {
+        switch (option[0])
+        {
+            case 'A':
+                showHeader(line);
+                list.displayList();
+                cout << line;
+                break;
+            case 'G':
+                cout << "Average (amount sold) $";
+                cout << setprecision(2) << fixed;
+                cout << list.average() << endl;
+                break;
+            case 'Y':
+                int year;
+                cout << "Enter year: " << endl;
+                cin >> year;
+                showHeader(line);
+                list.displayList(year);
+                cout << line;
+                break;
+        }
+        option = getOption();
+    }
+    cout << "Number of salespeople: " << list.getLength() << endl;
+}
